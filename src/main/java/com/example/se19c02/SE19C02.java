b96108b3819c5524544ffb53a2dba9e3bbba55e1
Tran Van Hoang <hoangtvh73@gmail.com>
2025-06-20 09:50:07 +0700

    1 commit

---
+++ src/main/java/com/example/se19c02/SE19C02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.se19c02;
+
+/**
+ *
+ * @author Admin
+ */
+public class SE19C02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
